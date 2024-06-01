cf71a464ad8aa8641cd30693733149748ac7ac66
lukshdaryanani <lukshdaryanani9@gmail.com>
2024-05-31 22:58:31 -0700

    navbar done

---
+++ nav.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>High-Level Navbar</title>
+<link rel="stylesheet" href="NAV.css">
+</head>
+<body>
+
+<nav class="navbar">
+  <img src="logo.jpg" alt="Logo" style="width:10%;" class="rounded-pill">
+    </a>
+      
+  <ul class="nav-links">
+    <li><a href="#">HOME</a></li>
+    <li><a href="#">ADMIN</a></li>
+    <li><a href="#">COUSTOMER</a></li>
+    
+    <li><a href="#">PANEL FOR LAWYERS</a></li>
+    <li><a href="#">CATEGORIES FOR LAWYERS</a></li>
+    <li><a href="#"> SIGN UP</a></li>
+   
+
+    <li><a href="#">CONTACT US</a></li>
+  </ul>
+  <div class="hamburger-menu">
+    <div class="bar"></div>
+    <div class="bar"></div>
+    <div class="bar"></div>
+  </div>
+</nav>
+
+<script src="nav.js"></script>
+</body>
+</html>
